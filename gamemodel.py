1a1aef5390ab5695837e1871afaff033fb862c8b
Anton Johansson <antonproxykonto@gmail.se>
2025-10-07 13:43:06 +0200

    Updated estetics.

--- gamemodel.py
+++ gamemodel.py
@@ -1,16 +1,15 @@
 # --- IMPORTS ---
 
+
 from math import sin, cos, radians
 import random
 
 
 # --- GAME MODEL ---
 
-class Game:
-    """
-    Create a game with a given size of cannon (length of sides) and projectiles (radius). 
-    """
 
+class Game:
+    """Create a game with a given size of cannon (length of sides) and projectiles (radius)."""
     def __init__(self, cannonSize, ballSize):
         self.cannonSize = cannonSize
         self.ballSize = ballSize
@@ -19,48 +18,51 @@ class Game:
         self.windSpeed = random.random() * 20 - 10
 
     def getPlayers(self):
-        """ A list containing both players """
+        """A list containing both players."""
         return self.players
 
     def getCannonSize(self):
-        """ The height/width of the cannon """
+        """The height/width of the cannon."""
         return self.cannonSize
 
     def getBallSize(self):
-        """ The radius of cannon balls """
+        """The radius of cannon balls."""
         return self.ballSize
     
     def getCurrentPlayer(self):
-        """ The current player, i.e. the player whose turn it is """
+        """The current player, i.e. the player whose turn it is."""
         return self.players[self.currentPlayer]
 
     def getOtherPlayer(self):
-        """ The opponent of the current player """
+        """The opponent of the current player."""
         return self.players[1 - self.currentPlayer]
     
     def getCurrentPlayerNumber(self):
-        """ The number (0 or 1) of the current player. This should be the position of the current player in getPlayers(). """
+        """The number (0 or 1) of the current player. This should be the position of the current player in getPlayers()."""
         return self.currentPlayer
     
     def nextPlayer(self):
-        """ Switch active player """
+        """Switch active player."""
         self.currentPlayer = 1 - self.currentPlayer
 
     def setCurrentWind(self, wind):
-        """ Set the current wind speed, only used for testing """
+        """Set the current wind speed, only used for testing."""
         self.windSpeed = float(wind)
     
     def getCurrentWind(self):
+        """Return current wind speed."""
         return self.windSpeed
 
     def newRound(self):
-        """ Start a new round with a random wind value (-10 to +10) """
+        """Start a new round with a random wind value (-10 to +10)."""
         self.windSpeed = random.random() * 20 - 10
 
 
 # --- PLAYER MODELS ---
 
-class Player: 
+
+class Player:
+    """Represents a player controlling a cannon in the game."""
     def __init__(self, game, isReversed, xPos, color):
         self.game = game
         self.isReversed = isReversed
@@ -113,33 +115,34 @@ class Player:
         return self.distance
 
     def getScore(self):
-        """ The current score of this player """
+        """The current score of this player."""
         return self.score
 
     def increaseScore(self):
-        """ Increase the score of this player by 1."""
+        """Increase the score of this player by 1."""
         self.score += 1
 
     def getColor(self):
-        """ Returns the color of this player (a string)"""
+        """Returns the color of this player (a string)."""
         return self.color
 
     def getX(self):
-        """ The x-position of the centre of this players cannon """
+        """The x-position of the centre of this players cannon."""
         return self.xPos
 
     def getAim(self):
-        """ The angle and velocity of the last projectile this player fired, initially (45, 40) """
+        """The angle and velocity of the last projectile this player fired, initially (45, 40)."""
         return self.angle, self.velocity
 
 
 # --- PROJECTILE MODELS ---
 
+
 class Projectile:
     """
         Constructor parameters:
         angle and velocity: the initial angle and velocity of the projectile 
-            angle 0 means straight east (positive x-direction) and 90 straight up
+        angle 0 means straight east (positive x-direction) and 90 straight up
         wind: The wind speed value affecting this projectile
         xPos and yPos: The initial position of this projectile
         xLower and xUpper: The lowest and highest x-positions allowed
@@ -159,7 +162,7 @@ class Projectile:
         """ 
         Advance time by a given number of seconds
         (typically, time is less than a second, 
-         for large values the projectile may move erratically)
+        for large values the projectile may move erratically)
         """
 
         # Compute new velocity based on acceleration from gravity/wind
@@ -182,13 +185,13 @@ class Projectile:
         self.xvel = xvel1
         
     def isMoving(self):
-        """ A projectile is moving as long as it has not hit the ground or moved outside the xLower and xUpper limits """
+        """A projectile is moving as long as it has not hit the ground or moved outside the xLower and xUpper limits."""
         return 0 < self.getY() and self.xLower < self.getX() < self.xUpper
 
     def getX(self):
-        """ The current x-position of the projectile". """
+        """The current x-position of the projectile"."""
         return self.xPos
 
     def getY(self):
-        """ The current y-position (height) of the projectile". Should never be below 0. """
+        """The current y-position (height) of the projectile". Should never be below 0."""
         return self.yPos
